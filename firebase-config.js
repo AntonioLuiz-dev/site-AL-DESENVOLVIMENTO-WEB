d6cad48d7ceea2907ac4efb6d76c9afffae6f616
AntonioLuiz-dev <antonioluiz.rx@gmail.com>
2026-03-25 09:52:39 -0300

    config firebase

--- firebase-config.js
+++ firebase-config.js
@@ -3,10 +3,10 @@
 // Siga o arquivo SETUP.md para instruções completas
 // =====================================================
 const firebaseConfig = {
-  apiKey:            "COLE_AQUI",
-  authDomain:        "COLE_AQUI.firebaseapp.com",
-  projectId:         "COLE_AQUI",
-  storageBucket:     "COLE_AQUI.appspot.com",
-  messagingSenderId: "COLE_AQUI",
-  appId:             "COLE_AQUI"
+  apiKey: "AIzaSyCSWZ9cVk6d5yx8Bi9wfdaq47OvQxh8HZk",
+  authDomain: "al-desenvolvimento-web.firebaseapp.com",
+  projectId: "al-desenvolvimento-web",
+  storageBucket: "al-desenvolvimento-web.firebasestorage.app",
+  messagingSenderId: "835554967504",
+  appId: "1:835554967504:web:bfa50f954b79b10a9b03d5"
 };
